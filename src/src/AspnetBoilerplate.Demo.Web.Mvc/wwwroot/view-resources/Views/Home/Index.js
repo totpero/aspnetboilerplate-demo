dde171fbaaea43ef13c25935c72d2178e0a2b288
totpero <www.totpe.ro@gmail.com>
2024-02-13 11:50:09 +0200

    https://github.com/aspnetboilerplate/aspnetboilerplate/issues/6884#issuecomment-1940967894

--- src/src/AspnetBoilerplate.Demo.Web.Mvc/wwwroot/view-resources/Views/Home/Index.js
+++ src/src/AspnetBoilerplate.Demo.Web.Mvc/wwwroot/view-resources/Views/Home/Index.js
@@ -5,7 +5,8 @@
 
     $('#TestDateAndTime').on("click",
         function(e) {
-            abp.services.app.test.testMethod({ testDate: "24.02.2024" });
+            //abp.services.app.test.testMethod({ testDate: "24.02.2024" });
+            abp.services.app.test.testMethod({ testDate: "02/24/2024" });
         });
 
 });
